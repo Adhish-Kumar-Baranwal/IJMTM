9bca18d00a85c666e2e309728b19ba7defc12bc7
Adhish-Kumar-Baranwal <adhish0725@gmail.com>
2025-03-22 22:39:58 +0530

    test changes

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,8 +1,5 @@
 import Hero from "../components/Hero/Hero";
 import NavBar from "../components/NavBar/NavBar";
-import FeaturedAuthor from "../components/sections/Featured/FeaturedAuthor";
-import FeaturedArticles from "../components/sections/Featured/FeaturedArticles";
-import CounterSection from "../components/sections/CounterSection/CounterSection";
 import Footer from "../Footer/Footer";
 
 const HomePage = () => {
