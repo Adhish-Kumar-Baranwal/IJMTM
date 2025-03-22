735374c69b2901020aa02b7c3614eb7d76baa754
Adhish-Kumar-Baranwal <adhish0725@gmail.com>
2025-03-22 22:38:23 +0530

    first changes

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -2,19 +2,16 @@ import Hero from "../components/Hero/Hero";
 import NavBar from "../components/NavBar/NavBar";
 import FeaturedAuthor from "../components/sections/Featured/FeaturedAuthor";
 import FeaturedArticles from "../components/sections/Featured/FeaturedArticles";
-import CounterSection from "../components/sections/CounterSection/CounterSection"
+import CounterSection from "../components/sections/CounterSection/CounterSection";
 import Footer from "../Footer/Footer";
 
 const HomePage = () => {
-    return (
+  return (
     <>
-        <NavBar />
-        <Hero />
-        <CounterSection />
-        <FeaturedAuthor />
-        <FeaturedArticles />
-        <Footer />
+      <NavBar />
+      <Hero />
+      <Footer />
     </>
-    );
-}
+  );
+};
 export default HomePage;
